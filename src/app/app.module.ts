614930adf35d255471c76b2dcf8bc1e00c76106a
elkebarrios <elkebarrios@gmail.com>
2022-10-16 20:58:32 -0300

    cree los componentes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,31 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavarComponent } from './componentes/navar/navar.component';
+import { SobreMiComponent } from './componentes/sobre-mi/sobre-mi.component';
+import { BannerComponent } from './componentes/banner/banner.component';
+import { ExperienciaComponent } from './componentes/experiencia/experiencia.component';
+import { EducacionComponent } from './componentes/educacion/educacion.component';
+import { HardysoftComponent } from './componentes/hardysoft/hardysoft.component';
+import { ProyectosComponent } from './componentes/proyectos/proyectos.component';
+import { ModalComponent } from './componentes/modal/modal.component';
+import { FooterComponent } from './componentes/footer/footer.component';
+import { Pagina404Component } from './componentes/pagina404/pagina404.component';
+import { Error404Component } from './componentes/pagina404/error404.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavarComponent
+    NavarComponent,
+    SobreMiComponent,
+    BannerComponent,
+    ExperienciaComponent,
+    EducacionComponent,
+    HardysoftComponent,
+    ProyectosComponent,
+    ModalComponent,
+    FooterComponent,
+    Pagina404Component,
+    Error404Component
   ],
   imports: [
     BrowserModule,
