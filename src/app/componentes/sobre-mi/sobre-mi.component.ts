dba5eed0af310d856c5dbdb689652d2b3d4385b5
elkebarrios <elkebarrios@gmail.com>
2022-10-23 19:02:42 -0300

    actualización 2310

--- src/app/componentes/sobre-mi/sobre-mi.component.ts
+++ src/app/componentes/sobre-mi/sobre-mi.component.ts
@@ -6,7 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./sobre-mi.component.css']
 })
 export class SobreMiComponent implements OnInit {
-
+  
   constructor() { }
 
   ngOnInit(): void {
